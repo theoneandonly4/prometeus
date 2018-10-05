1df22b5c25d1f483cc35a0a6cb91b91490345134
theoneandonly4 <pe.albinet@gmail.com>
2018-10-05 08:18:05 +0200

    20181005

--- page.js
+++ page.js
@@ -9,12 +9,46 @@ function changeUrl() {
 function changePath() {
   var context = JSON.parse(localStorage.context)
   document.getElementById('path')
-  path.innerHTML = context.path
+  path.innerHTML = context.path.replace(/\//g, ' > ')
 }
+
+function changeMain() {
+  var context = JSON.parse(localStorage.context)
+  var items = JSON.parse(localStorage.items)
+  var rootNode = context.path.substr(1)
+  var i
+  if (rootNode.indexOf('/') > -1) {
+    rootNode = rootNode.substr(0, rootNode.indexOf('/'))
+  }
+  switch (rootNode) {
+    case 'Users':
+        for (i = 0; i < items.length; i++) {
+          if (items[i].type == 'User') {
+            displayItem(items[i])
+          }
+        }
+      break;
+    case 'Home':
+
+      break;
+    case 'Orgas':
+
+      break;
+    case 'Projects':
+
+      break;
+    default:
+
+  }
+}
+
+function displayItem(item) {
+
+}
+
 
 function page() {
-changeUrl()
-changePath()
+  changeUrl()
+  changePath()
+  changeMain()
 }
-
-page()
--- script.js
+++ script.js
@@ -10,14 +10,18 @@ function generateItemId() {
     var items = JSON.parse(localStorage.items)
     var i
     var ids =[]
-    var check
+    var check = true
     for (i = 0; i < items.length; i++) {
       ids.push(items[i].id)
     }
     i = 0
-    while (!check) {
-      check = ids.indexOf(i) > -1
-      i++
+    while (check) {
+      if (ids.indexOf(i) > -1) {
+        i++
+      }
+      else {
+        check = false
+      }
     }
     return i
   }
@@ -33,7 +37,7 @@ function Item(parent, type) {
   }
   else {
     this.type = type
-    this.name = 'New' + type
+    this.name = 'New ' + type
   }
 }
 
@@ -51,22 +55,23 @@ function dataInitialize() {
   var user
 
   if (!localStorage.context) {
-    console.log('ok')
     context = {
       mode: 'local',
-      path: '/home',
+      path: '/Users',
       user: 'New User'
     }
 
-    user = new Item()
-
+    user = new Item('Users', 'User')
+    items.push(user)
     localStorage.context = JSON.stringify(context)
+    localStorage.items =   JSON.stringify(items)
   }
 }
 
 
 function prometeus() {
   dataInitialize()
+  page()
 }
 
 prometeus()
